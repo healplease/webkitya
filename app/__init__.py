d01b27fcda9b49b095a58d2df44a1fd93c735271
Alexey <gavaalex2012@gmail.com>
2021-08-31 15:35:10 +0300

    Development (#10)
    * Refactoring/project skeleton (#1)
    
    * add project sceletom part 1: dockerfile configs pre-commit
    
    * docker compose mongo
    
    * Feature/models (#2)
    
    * workind db?
    
    * mongodb working
    
    * imgur albums (#3)
    
    * procfile and requirements (#5)
    
    * template fixes (#7)
    
    * Feature/admin page (#9)
    
    * mongodb and admin page and dev options
    
    * flask moment and admin page
    
    * admin albums
    
    * at this point i dont really know what i'm doing
    
    * other settings

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,55 @@
+from urllib.parse import urlencode
+
 import dotenv
+import flask_bootstrap
 from flask import Flask
-# from flask_mongoengine import MongoEngine
-from flask_bootstrap import Bootstrap
+from flask_mongoengine import MongoEngine
 from flask_wtf import CSRFProtect
+from flask_moment import Moment
 
 from config import environment_configs
 
-from app.views import blueprints_to_register
+from app.views import blueprints_to_register, blueprints_to_register_dev_only
 
 dotenv.load_dotenv()
 
-# mongoengine = MongoEngine()
-bootstrap = Bootstrap()
-csrf = CSRFProtect()
+# bootstrap 4.2.1 supports spinners
+flask_bootstrap.BOOTSTRAP_VERSION = "4.2.1"
+flask_bootstrap.POPPER_VERSION = "1.14.6"
+flask_bootstrap.JQUERY_VERSION = "3.6.0"
+
+mongoengine = MongoEngine()
+bootstrap = flask_bootstrap.Bootstrap()
+csrf_protect = CSRFProtect()
+moment = Moment()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(f"config.{environment_configs[app.env].__name__}")
 
-    # mongoengine.init_app(app)
+    # setting DB connection URL
+    mongodb_settings = app.config.get_namespace("MONGODB_")
+    mongodb_settings["port"] = f":{mongodb_settings.get('port')}" if mongodb_settings.get("port") else ""
+    mongodb_settings["arguments"] = (
+        f"?{urlencode(mongodb_settings.get('arguments'))}" if mongodb_settings.get("arguments") else ""
+    )
+    app.config["MONGODB_HOST"] = "{protocol}://{username}:{password}@{host}{port}/{db}{arguments}".format(
+        **mongodb_settings
+    )
+
+    app.logger.info(f"Connection to database: {mongodb_settings['db']}")
+
     bootstrap.init_app(app)
-    csrf.init_app(app)
+    mongoengine.init_app(app)
+    csrf_protect.init_app(app)
+    moment.init_app(app)
 
     for blueprint in blueprints_to_register:
         app.register_blueprint(blueprint)
+
+    if app.env == "local":
+        for blueprint in blueprints_to_register_dev_only:
+            app.register_blueprint(blueprint)
 
     return app
--- app/views/main.py
+++ app/views/main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, current_app, render_template
 
+from app.models import Settings
 from app.domain.imgur import get_album_images
 
 main_bp = Blueprint("main", __name__)
@@ -7,9 +8,15 @@ main_bp = Blueprint("main", __name__)
 
 @main_bp.route("/")
 def index():
-    images = get_album_images(
-        album_id=current_app.config["IMGUR_ALBUM_NAME"],
-        client_id=current_app.config["IMGUR_CLIENT_ID"],
-        client_secret=current_app.config["IMGUR_CLIENT_SECRET"]
-    )
-    return render_template("index.html", images=images)
+    images = []
+    settings = Settings.get(env=current_app.env)
+    album_ids = [x.album_id for x in settings.albums] if settings else []
+
+    for album_id in album_ids:
+        images.extend(get_album_images(
+            album_id=album_id,
+            client_id=current_app.config["IMGUR_CLIENT_ID"],
+            client_secret=current_app.config["IMGUR_CLIENT_SECRET"]
+        ))
+
+    return render_template("index.html", images=images, settings=settings)

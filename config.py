c2902666c6f8eea292898c243ee46e3a85138958
Alexey <gavaalex2012@gmail.com>
2021-08-31 15:31:53 +0300

    Feature/admin page (#9)
    * mongodb and admin page and dev options
    
    * flask moment and admin page
    
    * admin albums
    
    * at this point i dont really know what i'm doing
    
    * other settings

--- config.py
+++ config.py
@@ -6,7 +6,15 @@ class Config():
 
     IMGUR_CLIENT_ID = os.environ.get("IMGUR_CLIENT_ID")
     IMGUR_CLIENT_SECRET = os.environ.get("IMGUR_CLIENT_SECRET")
-    IMGUR_ALBUM_NAME = os.environ.get("IMGUR_ALBUM_NAME")
+
+    MONGODB_USERNAME = os.environ.get("FLASK_MONGODB_USER")
+    MONGODB_PASSWORD = os.environ.get("FLASK_MONGODB_PASSWORD")
+    MONGODB_HOST = os.environ.get("FLASK_MONGODB_HOST")
+    MONGODB_PORT = os.environ.get("FLASK_MONGODB_PORT")
+    MONGODB_DB = "kitya"
+
+    MONGODB_PROTOCOL = "mongodb+srv"
+    MONGODB_ARGUMENTS = {"retryWrites": "true", "w": "majority"}
 
 
 class LocalConfig(Config):
